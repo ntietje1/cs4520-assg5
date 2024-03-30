44318b8bd8f6171186c6ad52f801c25cbc3a285e
Nicholas Tietje <tietje.n@northeastern.edu>
2024-03-30 11:13:23 -0400

    implemented loading circle & "no items found" text

--- app/src/main/java/com/cs4520/assignment5/ui/products/ProductScreen.kt
+++ app/src/main/java/com/cs4520/assignment5/ui/products/ProductScreen.kt
@@ -1,13 +1,20 @@
 package com.cs4520.assignment5.ui.products
 
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.material.CircularProgressIndicator
+import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
@@ -17,14 +24,35 @@ import com.cs4520.assignment5.ui.products.components.ProductList
 @Composable
 fun ProductScreen(viewModel: ProductViewModel = viewModel(factory = ProductViewModel.Factory)) {
     val state by viewModel.state.collectAsState()
-    Column(
-        modifier = Modifier.fillMaxSize()
+    Text("Products", style = MaterialTheme.typography.h4, modifier = Modifier.padding(16.dp))
+    Box(
+        modifier = Modifier
+            .fillMaxSize()
     ) {
-        Text(
-            modifier = Modifier.fillMaxWidth().height(50.dp),
-            text = "Products",
-            fontWeight = FontWeight.Bold
+        if (state.isLoading) {
+            CircularProgressIndicator(
+                modifier = Modifier
+                    .size(100.dp)
+                    .padding(5.dp)
+                    .align(Alignment.Center)
             )
-        ProductList(products = state.products)
+        }
+        Column(
+            modifier = Modifier.fillMaxSize(),
+            verticalArrangement = Arrangement.Top,
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            ProductList(products = state.products)
+            if (state.products.isEmpty() && !state.isLoading) {
+                Text(
+                    text = "No products found",
+                    style = MaterialTheme.typography.body1,
+                    fontWeight = FontWeight.Bold,
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(16.dp)
+                )
+            }
+        }
     }
 }
