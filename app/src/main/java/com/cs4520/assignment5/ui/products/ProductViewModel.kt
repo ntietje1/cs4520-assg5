3675dce56f81ec8ef73914b1e27aa1b9a3cd0cb0
Nicholas Tietje <tietje.n@northeastern.edu>
2024-03-30 11:13:40 -0400

    implemented loading circle & "no items found" text

--- app/src/main/java/com/cs4520/assignment5/ui/products/ProductViewModel.kt
+++ app/src/main/java/com/cs4520/assignment5/ui/products/ProductViewModel.kt
@@ -3,21 +3,36 @@ package com.cs4520.assignment5.ui.products
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
 import androidx.lifecycle.ViewModelProvider.AndroidViewModelFactory.Companion.APPLICATION_KEY
+import androidx.lifecycle.viewModelScope
 import androidx.lifecycle.viewmodel.CreationExtras
 import com.cs4520.assignment1.data.Product
 import com.cs4520.assignment5.application.App
 import com.cs4520.assignment5.data.db.ProductRepository
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.launch
 
 class ProductViewModel(private val repository: ProductRepository) : ViewModel() {
     private val _state = MutableStateFlow(ProductState())
     val state = _state.asStateFlow()
 
-    private val _isLoading = MutableStateFlow(true)
-    val isLoading get() = _isLoading.asStateFlow()
+    init {
+        getProducts()
+    }
 
-    fun updateProducts(products: List<Product>) {
+    private fun getProducts() {
+        viewModelScope.launch {
+            updateIsLoading(true)
+            updateProducts(repository.getProducts())
+            updateIsLoading(false)
+        }
+    }
+
+    private fun updateIsLoading(isLoading: Boolean) {
+        _state.value = _state.value.copy(isLoading = isLoading)
+    }
+
+    private fun updateProducts(products: List<Product>) {
         _state.value = _state.value.copy(products = products)
     }
 
